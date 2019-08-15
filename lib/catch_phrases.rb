4a53a7ad1f15641a9579bafd812f32523980e973
Glenn A Doherty <gadz@earthlink.net>
2019-08-15 21:12:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,5 +8,8 @@ def toadstool
   puts status
 end
 def link
-  puts "It's Dangerous To Go Alone! Take This"
+  puts "It's Dangerous To Go Alone! Take This."
+end
+def any_phrase (phrase)
+  puts phrase
 end
